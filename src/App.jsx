afe701d8d2b0525a22f189f3908f3efe4dfde92c
EVANDRO DE MACEDO SANTANA <101930026+evandromsantana@users.noreply.github.com>
2022-07-03 19:20:25 -0300

    terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-function App() {
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import './global.css';
+
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      
+      <Post 
+        author="Evandro Santana" 
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur iure adipisci maxime totam, eligendi eaque voluptates. Non quae laudantium facere, illum dolores, eaque quaerat cum, ducimus ipsum debitis sed voluptatem?" 
+      />
+      <Post
+        author="Aurora Macedo"
+        content="Um novo post muito legal" 
+      />
+      
+    </div>
+    
   )
 }
 
-export default App
