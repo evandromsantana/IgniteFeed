4090eda0a196e263ede497e52d1b9669558ed272
EVANDRO DE MACEDO SANTANA <101930026+evandromsantana@users.noreply.github.com>
2022-07-04 20:25:28 -0300

    QuatoCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header';
-import { Post } from './Post';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -9,17 +12,20 @@ export function App() {
     <div>
       <Header />
       
-      <Post 
-        author="Evandro Santana" 
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur iure adipisci maxime totam, eligendi eaque voluptates. Non quae laudantium facere, illum dolores, eaque quaerat cum, ducimus ipsum debitis sed voluptatem?" 
-      />
-      <Post
-        author="Aurora Macedo"
-        content="Um novo post muito legal" 
-      />
-      
-    </div>
-    
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Evandro Santana" 
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur iure adipisci maxime totam, eligendi eaque voluptates. Non quae laudantium facere, illum dolores, eaque quaerat cum, ducimus ipsum debitis sed voluptatem?" 
+          />
+          <Post
+            author="Aurora Macedo"
+            content="Um novo post muito legal" 
+          />
+        </main>
+      </div>      
+    </div>    
   )
 }
 
